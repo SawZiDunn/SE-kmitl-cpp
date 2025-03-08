ec94cc48b20b1c1d17a75b10aa93249bbde34462
SawZiDunn <szidunn199@gmail.com>
2025-03-08 17:33:51 +0700

    lab-12

---
+++ lab12/no1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Book {
+    public:
+        Book(string bookID, double baseFineRate): bookID{bookID}, baseFineRate{baseFineRate} {}
+        virtual void displayInfo() = 0;
+        
+        
+    protected:
+        string bookID;
+        double baseFineRate;
+
+        virtual double calculateFine(int daysOverdue) = 0;
+
+        
+        
+
+
+};
+
+class Textbook: public Book {
+    public:
+        Textbook(string bookID, double baseFineRate, string subject): Book::Book(bookID, baseFineRate), subject{subject} {}
+
+        void displayInfo() override{
+            cout << "ID: " << bookID << endl;
+            cout << "Base Fine Rate: $" << baseFineRate << endl;
+            cout << "Subject: " << subject << endl;
+            cout << "Fine for overdue: $" << calculateFine(5) << endl;
+        }
+
+    private:
+        string subject;
+
+        double calculateFine(int daysOverdue) override {
+            return Book::baseFineRate + (daysOverdue * 0.5);
+        }
+
+        
+};
+
+class Novel: public Book {
+    public:
+        Novel(string bookID, double baseFineRate, string genre): Book(bookID, baseFineRate), genre{genre} {}
+
+        void displayInfo() override{
+            cout << "ID: " << bookID << endl;
+             cout << "Base Fine Rate: $" << baseFineRate << endl;
+            cout << "Genre: " << genre << endl;
+            cout << "Fine for overdue: $" << calculateFine(5) << endl;
+        }
+
+    private:
+        string genre;
+
+        double calculateFine(int daysOverdue) override {
+            return Book::baseFineRate + (daysOverdue * 0.2);
+        }
+
+        
+};
+
+int main() {
+    vector<Book*> books;
+
+    books.push_back(new Textbook{"T001", 1, "Physics"});
+    books.push_back(new Novel{"N001", 0.5, "Mystery"});
+
+    for (Book* book : books) {
+        book -> displayInfo();
+    }
+
+    for (Book* book : books) {
+        delete book;
+    }
+    
+    return 0;
+}
---
+++ lab12/no2.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class CarbonEmitter {
+public:
+    CarbonEmitter(string uniqueId) : id(uniqueId) {}
+    virtual double getCarbonFootprint() = 0;
+    virtual void displayDetails() = 0;
+    string getId() const { return id; }
+
+protected:
+    string id;
+};
+
+class Factory: public CarbonEmitter {
+public:
+    Factory(string id, double prodHours, double energyHr, double wasteHr) 
+        : CarbonEmitter(id), productionHours(prodHours), 
+          energyPerHour(energyHr), wastePerHour(wasteHr) {}
+
+    double getCarbonFootprint() override {
+        return (energyPerHour * productionHours * 0.5) + (wastePerHour * productionHours * 0.2);
+    }
+
+    void displayDetails() override {
+        cout << "Factory Details:" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Production Hours: " << productionHours << endl;
+        cout << "Energy per Hour: " << energyPerHour << endl;
+        cout << "Waste per Hour: " << wastePerHour << endl;
+        cout << "Carbon Footprint: " << getCarbonFootprint() << " kg CO2" << endl;
+    }
+
+protected:
+    double productionHours;
+    double energyPerHour;
+    double wastePerHour;
+};
+
+class Car: public CarbonEmitter {
+public:
+    Car(string id, double miles, double efficiency) 
+        : CarbonEmitter(id), milesDriven(miles), fuelEfficiency(efficiency) {}
+
+    double getCarbonFootprint() override {
+        return (milesDriven / fuelEfficiency) * 8.8;
+    }
+
+    void displayDetails() override {
+        cout << "Car Details:" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Miles Driven: " << milesDriven << endl;
+        cout << "Fuel Efficiency: " << fuelEfficiency << endl;
+        cout << "Carbon Footprint: " << getCarbonFootprint() << " kg CO2" << endl;
+    }
+
+protected:
+    double milesDriven;
+    double fuelEfficiency;
+};
+
+class ElectricCar: public Car {
+public:
+    ElectricCar(string id, double miles, double efficiency, double batCapacity, double chargeEff)
+        : Car(id, miles, efficiency), batteryCapacity(batCapacity), chargeEfficiency(chargeEff) {}
+
+    double getCarbonFootprint() override {
+        return (milesDriven / fuelEfficiency) * (1 / chargeEfficiency) * 0.5;
+    }
+
+    void displayDetails() override {
+        cout << "Electric Car Details:" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Miles Driven: " << milesDriven << endl;
+        cout << "Fuel Efficiency: " << fuelEfficiency << endl;
+        cout << "Battery Capacity: " << batteryCapacity << endl;
+        cout << "Charge Efficiency: " << chargeEfficiency << endl;
+        cout << "Carbon Footprint: " << getCarbonFootprint() << " kg CO2" << endl;
+    }
+
+protected:
+    double batteryCapacity;
+    double chargeEfficiency;
+};
+
+class Building: public CarbonEmitter {
+public:
+    Building(string id, double energy, int occupants) 
+        : CarbonEmitter(id), energyUsage(energy), numberOfOccupants(occupants) {}
+
+    double getCarbonFootprint() override {
+        return (energyUsage / static_cast<double>(numberOfOccupants)) * 0.5 * numberOfOccupants;
+    }
+
+    void displayDetails() override {
+        cout << "Building Details:" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Energy Usage: " << energyUsage << endl;
+        cout << "Number of Occupants: " << numberOfOccupants << endl;
+        cout << "Carbon Footprint: " << getCarbonFootprint() << " kg CO2" << endl;
+    }
+
+protected:
+    double energyUsage;
+    int numberOfOccupants;
+};
+
+class Bicycle: public CarbonEmitter {
+public:
+    Bicycle(string id, double miles, double manuEmissions) 
+        : CarbonEmitter(id), milesRidden(miles), manufacturingEmissions(manuEmissions) {}
+
+    double getCarbonFootprint() override {
+        return manufacturingEmissions / 365;
+    }
+
+    void displayDetails() override {
+        cout << "Bicycle Details:" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Miles Ridden: " << milesRidden << endl;
+        cout << "Manufacturing Emissions: " << manufacturingEmissions << endl;
+        cout << "Carbon Footprint: " << getCarbonFootprint() << " kg CO2" << endl;
+    }
+
+protected:
+    double milesRidden;
+    double manufacturingEmissions;
+};
+
+int main() {
+    // Create a vector to store CarbonEmitter pointers
+    vector<CarbonEmitter*> carbonEmitters;
+
+    // Validate inputs and get user input for each type of object
+    Factory* factory = new Factory("FAC001", 10, 5.5, 0.3);
+    carbonEmitters.push_back(factory);
+
+    Car* car = new Car("CAR001", 100, 25);
+    carbonEmitters.push_back(car);
+
+    ElectricCar* electricCar = new ElectricCar("ECAR001", 80, 4, 50, 0.9);
+    carbonEmitters.push_back(electricCar);
+
+    Building* building = new Building("BLD001", 500, 10);
+    carbonEmitters.push_back(building);
+
+    Bicycle* bicycle = new Bicycle("BIC001", 20, 250);
+    carbonEmitters.push_back(bicycle);
+
+    double totalCarbonFootprint = 0.0;
+    cout << "Carbon Footprint Calculation System" << endl;
+    cout << "-----------------------------------" << endl;
+
+    for (CarbonEmitter* emitter : carbonEmitters) {
+        emitter->displayDetails();
+        totalCarbonFootprint += emitter->getCarbonFootprint();
+        cout << endl;
+    }
+
+    cout << "Total Carbon Footprint: " << fixed << setprecision(2) << totalCarbonFootprint << " kg CO2" << endl;
+
+    for (CarbonEmitter* emitter : carbonEmitters) {
+        delete emitter;
+    }
+
+    return 0;
+}
